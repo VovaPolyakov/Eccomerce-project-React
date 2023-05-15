bfa01f7e7c7dbf1257f37ad403d3a396cd7c64dd
Vladimir <1337vovalenovo@gmail.com>
2023-05-15 21:40:56 +0300

    added a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
-import './App.css';
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return <>
-  
+      <NavBar/>
   </>;
 }
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo from '../../assets/images/logo.png';
+import styles from './NavBar.module.scss'
+
 
 const NavBar = () => {
+  
   return (
-    <div>
-      
+    <div className={styles.header}>
+
     </div>
   )
 }
