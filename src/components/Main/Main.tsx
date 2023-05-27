168be80484ad275d64f00906d4a2ffe46f8a4cd7
ossofi <osinskaasona@gmao;.com>
2023-05-27 16:32:28 +0300

    sofi

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import { Routes ,Route } from 'react-router-dom';
 // const About = React.lazy(() => import('../About/About'));
 import About from "../About/About";
+import Concept from "../Concept/Concept"
+import Contacts from "../Contacts/Contacts"
 
 const Main = () => {
   return (
     <Routes>
         <Route path='/about' element={<About/>}/>
-        <Route path='/concept'/>
+        <Route path='/concept' element={<Concept/>}/>
         <Route path='/sorting'/>
         <Route path='/map'/>
         <Route path='/socia-lvalue'/>
         <Route path='/services'/>
-        <Route path='/contacts'/> 
+        <Route path='/contacts' element={<Contacts/>}/> 
         <Route path='/shop'/>
     </Routes>
   )
