f673af8b845f460833f94c5443bec9467596b1e6
Vladimir <1337vovalenovo@gmail.com>
2023-05-27 16:38:08 +0300

    added a /shop page

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -3,17 +3,19 @@ import styles from '../../styles/Shop.module.scss'
 import cart from '../../assets/images/bx_basket.png'
 import { Link } from 'react-router-dom'
 import ShopRouter from './ShopRouter'
+import Default from './Default'
 
 const Shop = () => {
   return (
     <div className={styles.shop}>
         <div className={styles.row}>
             <div className={styles.shop_buttons}>
-                <button className={styles.button_men}><Link to='/shop/men'>Мужчинам</Link></button>
-                <button className={styles.button_women}><Link to='/shop/women'>Женщинам</Link></button>
-                <button className={styles.button_sale}><Link to='/shop/sale'>Sale</Link></button>
-                <img className={styles.shop_cart} src={cart}></img>
+                <Link className={styles.shop_link} to='/shop/men'><button className={styles.button_men}>Мужчинам</button></Link>
+                <Link className={styles.shop_link} to='/shop/women'><button className={styles.button_women}>Женщинам</button></Link>
+                <Link className={styles.shop_link} to='/shop/sale'><button className={styles.button_sale}>Sale</button></Link>
+                <img alt='cart' className={styles.shop_cart} src={cart}></img>
             </div>
+            <Default/>
             <ShopRouter/>
         </div>
     </div>
--- src/components/Shop/ShopRouter.tsx
+++ src/components/Shop/ShopRouter.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Routes ,Route } from 'react-router-dom';
+import Default from './Default';
 
 const ShopRouter = () => {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path='/shop/men'/>
+      <Route path='/shop/women'/>
+      <Route path='/shop/sale'/>
+    </Routes>
   )
 }
 
