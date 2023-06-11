8bffc09131e2185455d3702e3b50a3f63e48d33f
ossofi <72688992+ossofi@users.noreply.github.com>
2023-06-11 12:29:09 +0300

    form done (#12)

--- src/components/Services/Done.tsx
+++ src/components/Services/Done.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import fulltrash from '../../assets/images/fulltrash.png'
 import styles from '../../styles/Done.module.scss'
-import { Routes, Route } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import dude from '../../assets/images/dude.png'
+import {useState} from 'react'
 
 
-
-const Done = () => {
+const Done = ({address, number}: any) => {
+  const location = useLocation()
+  console.log(location)
   return (
+
     <div className={styles.done}>
     <div className={styles.block4}>
     <div className={styles.block4_text}>
@@ -16,13 +19,14 @@ const Done = () => {
         <div className={styles.b4_t2}>Наша команда вывезет и отсортирует ваш мусор</div>
         <div className={styles.price}>ЦЕНА: <b>4.50 руб.</b></div>
         <div className={styles.yourinfo}>
-        <div className={styles.youraddress}>Пр-т Независимости 120 кв 10</div>
-        <div className={styles.yournumber}>+375295686709</div>
+        <div className={styles.youraddress}>{location.state.address}</div>
+        <div className={styles.yournumber}>{location.state.number}</div>
         </div>
         <button className={styles.button4}>Курьер в пути</button>
     </div>
     <img src={dude} className="dude" alt=''/> 
     </div></div>
+    
   )
 }
 
